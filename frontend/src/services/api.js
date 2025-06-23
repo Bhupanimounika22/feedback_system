b74c4a344b728db957ba34de16ef494d4712c461
Mounika <127584838+Bhupanimounika22@users.noreply.github.com>
2025-06-23 15:01:03 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://feedback-backend-0ziu.onrender.com/api';
+const API_URL = 'https://feedback-backend-0ziu.onrender.com/api' ||  'http://localhost:5001/api';
 
 const getAuthHeaders = () => {
     const token = localStorage.getItem('token');
